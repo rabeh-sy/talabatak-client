5fbbd71fdc20dcc9893187daf1c0ff7c003d2ae4
khaled-badenjki <m.k.badenjki2@gmail.com>
2025-08-22 20:14:18 +0300

    fixes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      new URL('http://localhost/**'),
+      new URL('http://localhost:3000/**'),
       new URL('https://qr-service.rabeh.sy/**')
     ],
   },
